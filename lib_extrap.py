ef7dd86bc31b361709618f6b9234b1441e6d4153
jim2 <jacob.monroe@nist.gov>
2020-03-02 16:17:20 -0500

    Added system size (single dimension length L) dependence to IG model.

--- lib_extrap.py
+++ lib_extrap.py
@@ -1162,94 +1162,95 @@ class VolumeInterpModel(InterpModel):
 
 
 class IGmodel:
-  """Defines a 1D ideal gas in an external field. The position, x, may vary from 0 to 1,
+  """Defines a 1D ideal gas in an external field. The position, x, may vary from 0 to L,
      with the field acting linearly on x, U(x) = a*x, where for simplicity we let a=1.
      This is a useful class to use for testing.
   """
 
   #Define some symbols and functions used across the class
   #All such classes will have identical symbols and functions, which is desirable here
-  #If had parameters for the x dimension (say L) or field strength, a, would need to
-  #make these methods object specific.
-  b = symbols('b')
-  avgXsym = (1/b) - 1/(exp(b) - 1)
-  avgXlambdify = lambdify(b, avgXsym, "numpy")
+  #Because volume is of secondary interest, set default parameter for this so that
+  #it does not need to be specified (keeps older code compatible, too)
+  b, l = symbols('b l')
+  avgXsym = (1/b) - l/(exp(b*l) - 1)
+  avgXlambdify = lambdify([b, l], avgXsym, "numpy")
 
   @classmethod
-  def avgX(cls, B):
+  def avgX(cls, B, L=1.0):
     """Average position x at the inverse temperature B
     """
-    return cls.avgXlambdify(B)
+    return cls.avgXlambdify(B, L)
 
   @classmethod
-  def varX(cls, B):
+  def varX(cls, B, L=1.0):
     """Variance in position, x at the inverse temperature B
     """
     term1 = 1.0/(B**2)
-    term2 = np.exp(B)/((np.exp(B) - 1)**2)
+    term2 = (L**2)*np.exp(B*L)/((np.exp(B*L) - 1)**2)
     return (term1 - term2)
 
   @classmethod
-  def PofX(cls, x, B): #This will also give P(U) exactly for a single particle if a = 1
+  def PofX(cls, x, B, L=1.0): #This will also give P(U) exactly for single particle if a = 1
     """Canonical probability of position x for single particle at inverse temperature B
     """
     numer = B*np.exp(-B*x)
-    denom = 1.0 - np.exp(-B)
+    denom = 1.0 - np.exp(-B*L)
     return (numer / denom)
 
   @classmethod
-  def cdfX(cls, x, B): #Cumulative distribution function for X
+  def cdfX(cls, x, B, L=1.0): #Cumulative distribution function for X
     """Cumulative probability density for position x for single particle at inverse temperature B
     """
     numer = 1.0 - np.exp(-B*x)
-    denom = 1.0 - np.exp(-B)
+    denom = 1.0 - np.exp(-B*L)
     return (numer / denom)
 
   def __init__(self, nParticles=1000):
-    self.nP = nParticles
+    self.nP = nParticles #Number of particles
 
-  def sampleX(self, B, s):
+  def sampleX(self, B, s, L=1.0):
     """Samples s samples of x from the probability density at inverse temperature B
+       Does sampling based on inversion of cumulative distribution function
     """
     randvec = np.random.random(size=s)
-    randx = -(1.0/B)*np.log(1.0 - randvec*(1.0 - np.exp(-B)))
+    randx = -(1.0/B)*np.log(1.0 - randvec*(1.0 - np.exp(-B*L)))
     return randx
 
-  def sampleU(self, B, s=1000): #Really just resampling the sum of x values many times to get distribution of U for large N
+  def sampleU(self, B, s=1000, L=1.0): #Really just resampling the sum of x values many times to get distribution of U for large N
     """Samples s (=1000 by default) potential energy values from a system self.nP particles.
        Particle positions are randomly sampled with sampleX at the inverse temperature B.
     """
     randu = np.zeros(s)
     for i in range(s):
-        randu[i] = np.sum(self.sampleX(B, self.nP))
+        randu[i] = np.sum(self.sampleX(B, self.nP, L=L))
     return randu
 
-  def PofU(self, U, B): #Provides P(U) in the limit of a large number of particles (becomes Gaussian)
+  def PofU(self, U, B, L=1.0): #Provides P(U) in the limit of a large number of particles (becomes Gaussian)
     """In the large-N limit, the probability of the potential energy is Normal, so provides that
     """
-    avgU = self.nP*self.avgX(B)
-    stdU = np.sqrt(self.nP*self.varX(B))
+    avgU = self.nP*self.avgX(B, L=L)
+    stdU = np.sqrt(self.nP*self.varX(B, L=L))
     return norm.pdf(U, avgU, stdU)
 
-  def pertAnalytic(self, B, B0): #Really just the same as average of x, but it's a nice check of all the math
+  def pertAnalytic(self, B, B0, L=1.0): #Really just the same as average of x, but it's a nice check of all the math
     """Analytical perturbation of the system from B0 to B.
        Nice check to see if get same thing as avgX
     """
     def pertNumer(B, B0):
-      prefac = B0 / (1.0 - np.exp(-B0))
-      term1 = (1.0 - np.exp(-B)) / (B**2)
-      term2 = np.exp(-B) / B
+      prefac = B0 / (1.0 - np.exp(-B0*L))
+      term1 = (1.0 - np.exp(-B*L)) / (B**2)
+      term2 = L*np.exp(-B*L) / B
       return (prefac*(term1 - term2))
 
     def pertDenom(B, B0):
       prefac = B0 / B
-      numer = 1.0 - np.exp(-B)
-      denom = 1.0 - np.exp(-B0)
+      numer = 1.0 - np.exp(-B*L)
+      denom = 1.0 - np.exp(-B0*L)
       return (prefac*numer/denom)
 
     return (pertNumer(B, B0) / pertDenom(B, B0))
 
-  def extrapAnalytic(self, B, B0, order):
+  def extrapAnalytic(self, B, B0, order, L=1.0):
     """Analytical extrapolation from B0 to B at specified order.
        Same as if used infinite number of symbols, so only includes truncation error.
     """
@@ -1258,16 +1259,29 @@ class IGmodel:
     outval = 0.0
     for k in range(order+1):
         thisdiff = diff(self.avgXsym, self.b, k)
-        outvec[k] = thisdiff.subs(self.b, B0)
+        outvec[k] = thisdiff.subs({self.b:B0, self.l:L})
         outval += outvec[k]*(dBeta**k)/np.math.factorial(k)
     return (outval, outvec)
 
+  def extrapAnalyticVolume(self, L, L0, order, B=1.0):
+    """Analytical extrapolation from a reference system L0 to new L at specified order.
+       Must also specify inverse temperature B if want it to be other than 1.0
+    """
+    dL = L - L0
+    outvec = np.zeros(order+1)
+    outval = 0.0
+    for k in range(order+1):
+        thisdiff = diff(self.avgXsym, self.l, k)
+        outvec[k] = thisdiff.subs({self.b:B, self.l:L0})
+        outval += outvec[k]*(dL**k)/np.math.factorial(k)
+    return (outval, outvec)
+
   #Want to be able to create sample data set we can work with at a reference beta
-  def genData(self, B, nConfigs=100000):
+  def genData(self, B, nConfigs=100000, L=1.0):
     """Generates nConfigs data points of the model at inverse temperature beta.
        Returns are the average x values and the potential energy values of each data point.
     """
-    allX = self.sampleX(B, nConfigs*self.nP)
+    allX = self.sampleX(B, nConfigs*self.nP, L=L)
     allConfigs = np.reshape(allX, (nConfigs, self.nP))
     obsX = np.average(allConfigs, axis=1)
     obsU = np.sum(allConfigs, axis=1)
