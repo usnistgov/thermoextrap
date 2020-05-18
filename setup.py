057402337624b23a87fb1cec2bedcb1333141dbb
jim2 <jacob.monroe@nist.gov>
2020-05-18 10:31:26 -0400

    Added basic setup.py without dependency info.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='libExtrap',
+      version='0.1',
+      packages=['libextrap',],
+      description="Library of functions useful for thermodynamic extrapolation" \
+                  " and interpolation",
+      long_description=open('README.md').read(),
+      license=open('LICENSE').read(),
+     )
