8a9cfd75011f324d087e948011b1199f2565e969
wpk <william.krekelberg@nist.gov>
2020-12-15 16:51:12 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,30 +1,33 @@
 from setuptools import setup, find_packages
 
-setup(name='cmomy',
-      version='0.0.1',
-      description='Central (co)moment calculation',
-      long_description='Calculate and manipulate moments',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-      ],
-      keywords='data analysis',
-      url='https://github.com/wpk-nist-gov/cmomy',
-      author='William Krekelberg',
-      author_email='wpk@nist.gov',
-      license='NIST license https://www.nist.gov/director/licensing',
-      packages=find_packages(), #['cmomy'],
-      install_requires=[
-#          'numpy>=1.13.3',
-#          'pandas>=0.21.0',
-#          'matplotlib>=2.1.0',
-#          'seaborn>=0.8.0',
-      ],
-      # testing
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      test_suite='pytest',
-      include_package_data=True,
-      zip_safe=False)
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="cmomy",
+    version="0.0.1",
+    description="Central (co)moment calculation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="data analysis",
+    url="https://github.com/wpk-nist-gov/cmomy",
+    author="William Krekelberg",
+    author_email="wpk@nist.gov",
+    license="NIST license https://www.nist.gov/director/licensing",
+    # packages=['cmomy'],
+    packages=find_packages(), 
+    install_requires=["numpy", "numba", "xarray"],
+    # testing
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    test_suite="pytest",
+    include_package_data=True,
+    # zip_safe=False,
+    zip_safe=True,
+)
