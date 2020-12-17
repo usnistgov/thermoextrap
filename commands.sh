1afc35896a367ccacdd3dd68277d86d3a8c4d032
wpk <william.krekelberg@nist.gov>
2020-12-17 16:24:10 -0500

    added tox support, using tox-conda

--- commands.sh
+++ commands.sh
@@ -10,12 +10,17 @@ python setup.py sdist bdist_wheel
 # upload to pypi
 twine upload dist/*
 
-# create conda dist
+# create conda dist from pypi
 mkdir conda_dist
 grayskull pypi cmomy
-
 conda-build cmomy
 anaconda upload /path/to/cmomy
+
+
+# create conda dist from git
+cd recipe/github/
+conda-build .
+
 
 # run tests on pip/conda
 conda create -n cmomy_pip python=3.8 pytest
