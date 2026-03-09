bef661b1cb50e1a78f64271b053118e852327e52
wpk-nist-gov <wpk@nist.gov>
2026-03-09 13:34:51 -0400

    chore: add back missing environments

--- tools/numba_cache_tools.py
+++ tools/numba_cache_tools.py
@@ -8,7 +8,7 @@ import hashlib
 import os
 import platform
 import sys
-from functools import cached_property
+from functools import cached_property, lru_cache
 from importlib import import_module
 from pathlib import Path
 from typing import TYPE_CHECKING, cast
@@ -18,6 +18,16 @@ from numba.core.caching import UserProvidedCacheLocator
 
 if TYPE_CHECKING:
     from typing import Any
+
+# Used in hash below
+MACHINE_HASH = [
+    "numba",
+    numba.__version__,
+    platform.python_implementation().lower(),
+    ".".join(platform.python_version_tuple()[:-1]),
+    sys.platform,
+    platform.machine(),
+]
 
 
 class _Config:
@@ -54,28 +64,34 @@ class SharedHashCacheLocator(HashCacheLocator):
         This will reduce a file path that is too long, which can be a problem
         on some operating system (i.e. Windows 7).
         """
-        path = Path(py_file).resolve()
-        rel_dir = path.resolve().parent
+        return _shared_hash_suitable_cache_subpath(py_file)
 
-        for _ in range(100):
-            name = rel_dir.name
-            if name in {"src", "site-packages"}:
-                break
-            rel_dir = rel_dir.parent
-        else:
-            msg = f"Can't determine source path for file {py_file}"
-            raise ValueError(msg)
 
-        subpath = path.parent.relative_to(rel_dir)
-        package_version = import_module(subpath.parts[0]).__version__
+@lru_cache
+def _shared_hash_suitable_cache_subpath(py_file: str) -> str:
+    path = Path(py_file).resolve()
+    rel_dir = path.resolve().parent
 
-        return "_".join(
-            [
-                *subpath.parts,
-                config.magic,
-                package_version,
-                sys.platform,
-                platform.python_version(),
-                numba.__version__,
-            ]
-        )
+    for _ in range(100):
+        name = rel_dir.name
+        if name in {"src", "site-packages"}:
+            break
+        rel_dir = rel_dir.parent
+    else:
+        msg = f"Can't determine source path for file {py_file}"
+        raise ValueError(msg)
+
+    subpath = path.parent.relative_to(rel_dir)
+
+    package = subpath.parts[0]
+    package_version = import_module(package).__version__
+
+    return "-".join(
+        [
+            *subpath.parts,
+            config.magic,
+            package,
+            package_version,
+            *MACHINE_HASH,
+        ]
+    )
